71cdb34e1ff834cd97f4df1a74aa224ab7ffc944
Rittick Datta <drittick@gmail.com>
2020-09-25 14:04:07 +0530

    Burger Builder, Burger, Ingredients, Build Control

---
+++ burger-builder/src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/Burgeringredient';
+
+const burger = (props) => {
+  let transformIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + 1} type={igKey} />;
+    });
+  })
+  .reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
+
+  if( transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients</p>
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type='bread-top' />
+      {transformIngredients}
+      <BurgerIngredient type='bread-bottom' />
+    </div>
+  );
+};
+
+export default burger;
